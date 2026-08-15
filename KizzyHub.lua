c2627ceb65add19d077599e403e892c658733862
KizzyDaGoat <zacharychutcheson1.@gmail.com>
2026-08-15 06:33:02 -0400

    Update KizzyHub.lua

--- KizzyHub.lua
+++ KizzyHub.lua
@@ -1,12 +1,23 @@
+-- Services
 local CoreGui = game:GetService("CoreGui")
 local SoundService = game:GetService("SoundService")
 local RunService = game:GetService("RunService")
 
+-- 1. Create a ScreenGui parented directly to CoreGui
 local screenGui = Instance.new("ScreenGui")
 screenGui.Name = "JumpscareOverlay"
 screenGui.IgnoreGuiInset = true
+
+-- Protect the GUI from being deleted or blocked by PC executors/game scripts
+if syn and syn.protect_gui then
+    syn.protect_gui(screenGui)
+elseif protectgui then
+    protectgui(screenGui)
+end
+
 screenGui.Parent = CoreGui
 
+-- 2. Create an ImageLabel that stretches across the entire screen
 local imageLabel = Instance.new("ImageLabel")
 imageLabel.Name = "JumpscareImage"
 imageLabel.Size = UDim2.new(1, 0, 1, 0)
@@ -16,6 +27,7 @@ imageLabel.BackgroundTransparency = 0
 imageLabel.Image = "rbxassetid://10468371302"
 imageLabel.Parent = screenGui
 
+-- 3. Set up the looping sound
 local sound = Instance.new("Sound")
 sound.Name = "JumpscareAudio"
 sound.SoundId = "rbxassetid://73627747173553"
@@ -24,16 +36,17 @@ sound.Looped = true
 sound.Parent = SoundService
 sound:Play()
 
+-- 4. Continuous camera shake loop effect
 local camera = workspace.CurrentCamera
 local intensity = 0.5
 
 local connection
 connection = RunService.RenderStepped:Connect(function()
-    if screenGui.Parent then
-        local shakeX = math.random(-100, 100) * 0.001 * intensity
-        local shakeY = math.random(-100, 100) * 0.001 * intensity
-        camera.CFrame = camera.CFrame * CFrame.new(shakeX, shakeY, 0)
-    else
-        connection:Disconnect()
-    end
+	if screenGui.Parent then
+		local shakeX = math.random(-100, 100) * 0.001 * intensity
+		local shakeY = math.random(-100, 100) * 0.001 * intensity
+		camera.CFrame = camera.CFrame * CFrame.new(shakeX, shakeY, 0)
+	else
+		connection:Disconnect()
+	end
 end)
